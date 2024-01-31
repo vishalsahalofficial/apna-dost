858f723b1e7e68e4ecda9638333cc45693ef2039
vishalsahal <vsahal25@gmail.com>
2024-01-31 23:01:48 +0530

    Banner added

--- src/main/java/com/apnadost/ApnaDostApplication.java
+++ src/main/java/com/apnadost/ApnaDostApplication.java
@@ -10,7 +10,6 @@ public class ApnaDostApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApnaDostApplication.class, args);
-        System.out.println("HI");
     }
 }
 
